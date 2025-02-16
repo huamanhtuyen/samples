7b2690f274f12efa71af5efcbd5bb2292f358c28
huamanhtuyen <huamanhtuyen@gmail.com>
2025-02-16 22:21:40 +0700

    Update assets.dart

--- compass_app/app/lib/config/assets.dart
+++ compass_app/app/lib/config/assets.dart
@@ -2,7 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Lớp Assets chứa các đường dẫn đến các tài nguyên trong ứng dụng
 abstract final class Assets {
+  // Đường dẫn đến file activities.json
   static const activities = 'assets/activities.json';
+  // Đường dẫn đến file destinations.json
   static const destinations = 'assets/destinations.json';
 }
