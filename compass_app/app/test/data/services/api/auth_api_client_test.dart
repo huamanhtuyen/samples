a937b0b37040fd1756c4610f0c0d76d77015422c
huamanhtuyen <huamanhtuyen@gmail.com>
2025-02-18 09:16:50 +0700

    chạy test trên môi trường thật

--- compass_app/app/test/data/services/api/auth_api_client_test.dart
+++ compass_app/app/test/data/services/api/auth_api_client_test.dart
@@ -7,74 +7,38 @@
 //import 'package:compass_app/data/services/api/model/login_response/login_response.dart'; // Import model LoginResponse
 import 'package:flutter_test/flutter_test.dart'; // Import thư viện kiểm thử của Flutter
 //import 'package:mockito/mockito.dart'; // Import thư viện Mockito để tạo mock objects
-import 'package:shared_preferences/shared_preferences.dart'; // Import thư viện shared_preferences
+//import 'package:shared_preferences/shared_preferences.dart'; // Import thư viện shared_preferences
 import 'package:supabase_flutter/supabase_flutter.dart'; // Import thư viện Supabase cho Flutter
 //import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import thư viện dotenv để sử dụng biến môi trường
 
 //import '../../../../testing/utils/result.dart'; // Import tiện ích Result từ thư mục testing
 
 void main() async {
-  TestWidgetsFlutterBinding.ensureInitialized(); // Đảm bảo môi trường kiểm thử Flutter được khởi tạo
+  //TestWidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://hxetbhwcaqjfmfpnaiiq.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4ZXRiaHdjYXFqZm1mcG5haWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2NjYzNDAsImV4cCI6MjA1NTI0MjM0MH0.I-a7DHYtOVNt5oB9EUrfBwqff0bj8AruMlVab37ZyZY',
+  );
 
-  SharedPreferences.setMockInitialValues(
-    {},
-  ); // Thiết lập giá trị mặc định cho shared_preferences
+  final supabase = Supabase.instance.client;
 
   group('AuthApiClient', () {
-    // Tạo nhóm kiểm thử cho AuthApiClient
-
-    setUp(() async {});
-
     test('should post login', () async {
-      // Kiểm thử hàm login
-      //cái mong muốn trả về
-      // final loginResponse = LoginResponse(
-      //   // Tạo đối tượng LoginResponse mẫu
-      //   token: 'TOKEN',
-      //   userId: '123',
-      // );
-
-      //Khởi tạo kết nối đến supabase
-      await Supabase.initialize(
-        url: 'https://hxetbhwcaqjfmfpnaiiq.supabase.co',
-        anonKey:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4ZXRiaHdjYXFqZm1mcG5haWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2NjYzNDAsImV4cCI6MjA1NTI0MjM0MH0.I-a7DHYtOVNt5oB9EUrfBwqff0bj8AruMlVab37ZyZY',
-      );
-      // final supabaseClient = SupabaseClient(
-      //   'https://hxetbhwcaqjfmfpnaiiq.supabase.co',
-      //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4ZXRiaHdjYXFqZm1mcG5haWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2NjYzNDAsImV4cCI6MjA1NTI0MjM0MH0.I-a7DHYtOVNt5oB9EUrfBwqff0bj8AruMlVab37ZyZY',
-      // );
-      final supabase = Supabase.instance.client;
-
-      // final apiClient = AuthApiClient(
-      //   supabaseClient: supabaseClient,
-      // ); // Initialize apiClient
-
-      // final result = await apiClient.login(
-      //   // Gọi hàm login trên apiClient
-      //   const LoginRequest(
-      //     email: 'test1@gmail.com',
-      //     password: '123456',
-      //   ), // Truyền vào LoginRequest mẫu
-      // );
-
-      final response = await supabase.auth.signInWithPassword(
-        email: 'test1',
-        password: '123456',
-      );
-
-      // final response = await supabaseClient.auth.signInWithPassword(
-      //   email: 'test1@gmail.com',
-      //   password: '123456',
-      // );
-      print('response=');
-      print(response);
-      //print('Error: ${result.asError.error}');
-
-      // expect(
-      //   result.asOk.value,
-      //   loginResponse,
-      // ); // Kiểm tra kết quả trả về không null
+      String? token = '';
+      try {
+        final response = await supabase.auth.signInWithPassword(
+          email: 'test1@gmail.com',
+          password: '123456',
+        );
+        //print('Đăng nhập thành công: ${response.session?.toJson()}');
+        token = response.session?.accessToken;
+      } catch (error) {
+        print('Lỗi đăng nhập: ${error.toString()}');
+      }
+      // Kiểm tra token không null và không rỗng
+      expect(token, isNotNull);
+      expect(token, isNotEmpty);
     });
   });
 }
