2433e98d4c33c2c753435385a02821d94b82493b
huamanhtuyen <huamanhtuyen@gmail.com>
2025-03-03 13:56:42 +0700

    Update chuxe_screen.dart

--- compass_app/app/lib/ui/chuxe/widgets/chuxe_screen.dart
+++ compass_app/app/lib/ui/chuxe/widgets/chuxe_screen.dart
@@ -3,11 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../../routing/routes.dart';
 import '../view_models/chuxe_viewmodel.dart';
-import '../../core/ui/language_button.dart';
-import '../../auth/logout/widgets/logout_button_big.dart';
-import '../../auth/logout/view_models/logout_viewmodel.dart';
-import 'package:provider/provider.dart';
-import '../../core/ui/navigation_bar.dart'; // Import NavigationBars
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ChuXeScreen extends StatefulWidget {
   const ChuXeScreen({super.key, required this.viewModel});
@@ -20,45 +16,9 @@ class ChuXeScreen extends StatefulWidget {
 
 class _ExampleStaggeredAnimationsState extends State<ChuXeScreen>
     with SingleTickerProviderStateMixin {
-  late AnimationController
-  _drawerSlideController; // Điều khiển hoạt ảnh cho drawer
-  late int _selectedIndex = 0; // Add selectedIndex for NavigationBars
-
   @override
   void initState() {
     super.initState();
-
-    _drawerSlideController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 150), // Thời gian hoạt ảnh
-    );
-  }
-
-  @override
-  void dispose() {
-    _drawerSlideController.dispose(); // Giải phóng tài nguyên
-    super.dispose();
-  }
-
-  bool _isDrawerOpen() {
-    return _drawerSlideController.value == 1.0; // Kiểm tra drawer có mở không
-  }
-
-  bool _isDrawerOpening() {
-    return _drawerSlideController.status ==
-        AnimationStatus.forward; // Kiểm tra drawer đang mở
-  }
-
-  bool _isDrawerClosed() {
-    return _drawerSlideController.value == 0.0; // Kiểm tra drawer có đóng không
-  }
-
-  void _toggleDrawer() {
-    if (_isDrawerOpen() || _isDrawerOpening()) {
-      _drawerSlideController.reverse(); // Đóng drawer
-    } else {
-      _drawerSlideController.forward(); // Mở drawer
-    }
   }
 
   @override
@@ -69,64 +29,23 @@ class _ExampleStaggeredAnimationsState extends State<ChuXeScreen>
             context,
           ).scaffoldBackgroundColor, // Use theme background color
       appBar: _buildAppBar(context), // Pass context to _buildAppBar
-      body: Stack(
-        children: [_buildContent(), _buildDrawer()],
-      ), // Xây dựng nội dung và drawer
-      bottomNavigationBar: NavigationBars(
-        selectedIndex: _selectedIndex,
-        onSelectItem: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-
-          if (index == 0) context.go(Routes.home);
-        },
-      ), // Add NavigationBars at the bottom
+      body: Stack(children: [_buildContent()]), // Xây dựng nội dung và drawer
     );
   }
 
   PreferredSizeWidget _buildAppBar(BuildContext context) {
     return AppBar(
-      title: const Text('LogiW', style: TextStyle(color: Colors.black)),
+      title: SvgPicture.asset(
+        'assets/logo.svg',
+        height: 30, // Adjust the height as needed
+        fit: BoxFit.contain,
+      ),
       backgroundColor:
           Theme.of(
             context,
           ).appBarTheme.backgroundColor, // Use theme app bar color
       elevation: 0.0,
       automaticallyImplyLeading: false,
-      actions: [
-        // Nút ngôn ngữ
-        const LanguageButton(),
-        // Nút thông báo
-        IconButton(
-          onPressed: () {
-            context.go('/notifications'); // Điều hướng tới trang thông báo
-          },
-          icon: Icon(
-            Icons.notifications,
-            color: Theme.of(context).colorScheme.primary,
-          ),
-        ),
-        //nút menu
-        AnimatedBuilder(
-          animation: _drawerSlideController,
-          builder: (context, child) {
-            return IconButton(
-              onPressed: _toggleDrawer,
-              icon:
-                  _isDrawerOpen() || _isDrawerOpening()
-                      ? Icon(
-                        Icons.clear,
-                        color: Colors.black,
-                      ) // Icon khi drawer mở
-                      : Icon(
-                        Icons.menu,
-                        color: Theme.of(context).colorScheme.primary,
-                      ), // Icon khi drawer đóng
-            );
-          },
-        ),
-      ],
     );
   }
 
@@ -202,240 +121,4 @@ class _ExampleStaggeredAnimationsState extends State<ChuXeScreen>
       ),
     );
   } //end _buildContent
-
-  Widget _buildDrawer() {
-    return AnimatedBuilder(
-      animation: _drawerSlideController,
-      builder: (context, child) {
-        return FractionalTranslation(
-          translation: Offset(
-            1.0 - _drawerSlideController.value,
-            0.0,
-          ), // Dịch chuyển drawer
-          child:
-              _isDrawerClosed()
-                  ? const SizedBox()
-                  : const Menu(), // Hiển thị drawer khi mở
-        );
-      },
-    );
-  }
-}
-
-//nút menu trên phải
-class Menu extends StatefulWidget {
-  const Menu({super.key});
-
-  @override
-  State<Menu> createState() => _MenuState();
-}
-
-class _MenuState extends State<Menu> with SingleTickerProviderStateMixin {
-  static const _menuTitles = [
-    'Thông tin doanh nghiệp',
-    'Tài khoản',
-  ]; // Danh sách tiêu đề menu
-
-  static const _initialDelayTime = Duration(
-    milliseconds: 50,
-  ); // Thời gian trễ ban đầu
-  static const _itemSlideTime = Duration(
-    milliseconds: 250,
-  ); // Thời gian trượt của mỗi mục
-  static const _staggerTime = Duration(
-    milliseconds: 50,
-  ); // Thời gian trễ giữa các mục
-  static const _buttonDelayTime = Duration(
-    milliseconds: 150,
-  ); // Thời gian trễ của nút
-  static const _buttonTime = Duration(
-    milliseconds: 500,
-  ); // Thời gian hoạt ảnh của nút
-  final _animationDuration =
-      _initialDelayTime +
-      (_staggerTime * _menuTitles.length) +
-      _buttonDelayTime +
-      _buttonTime; // Tổng thời gian hoạt ảnh
-
-  late AnimationController _staggeredController; // Điều khiển hoạt ảnh
-  final List<Interval> _itemSlideIntervals =
-      []; // Danh sách khoảng thời gian hoạt ảnh cho các mục
-  late Interval _buttonInterval; // Khoảng thời gian hoạt ảnh cho nút
-
-  @override
-  void initState() {
-    super.initState();
-
-    _createAnimationIntervals(); // Tạo các khoảng thời gian hoạt ảnh
-
-    _staggeredController = AnimationController(
-      vsync: this,
-      duration: _animationDuration,
-    )..forward(); // Bắt đầu hoạt ảnh
-  }
-
-  void _createAnimationIntervals() {
-    for (var i = 0; i < _menuTitles.length; ++i) {
-      final startTime = _initialDelayTime + (_staggerTime * i);
-      final endTime = startTime + _itemSlideTime;
-      _itemSlideIntervals.add(
-        Interval(
-          startTime.inMilliseconds / _animationDuration.inMilliseconds,
-          endTime.inMilliseconds / _animationDuration.inMilliseconds,
-        ),
-      );
-    }
-
-    final buttonStartTime =
-        Duration(milliseconds: (_menuTitles.length * 50)) + _buttonDelayTime;
-    final buttonEndTime = buttonStartTime + _buttonTime;
-    _buttonInterval = Interval(
-      buttonStartTime.inMilliseconds / _animationDuration.inMilliseconds,
-      buttonEndTime.inMilliseconds / _animationDuration.inMilliseconds,
-    );
-    if (_buttonInterval.end > 1.0) {}
-  }
-
-  @override
-  void dispose() {
-    _staggeredController.dispose(); // Giải phóng tài nguyên
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          _buildFlutterLogo(),
-          _buildContent(),
-        ], // Xây dựng logo và nội dung
-      ),
-    );
-  }
-
-  Widget _buildFlutterLogo() {
-    return const Positioned(
-      right: -100,
-      child: Opacity(
-        opacity: 0.2,
-        child: FlutterLogo(size: 400),
-      ), // Logo Flutter
-    );
-  }
-
-  Widget _buildContent() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        const SizedBox(height: 16),
-        ..._buildListItems(), // Xây dựng các mục trong danh sách
-        const Spacer(),
-      ],
-    );
-  }
-
-  List<Widget> _buildListItems() {
-    final listItems = <Widget>[];
-    for (var i = 0; i < _menuTitles.length; ++i) {
-      listItems.add(
-        AnimatedBuilder(
-          animation: _staggeredController,
-          builder: (context, child) {
-            final animationPercent = Curves.easeOut.transform(
-              _itemSlideIntervals[i].transform(_staggeredController.value),
-            );
-            final opacity = animationPercent;
-            final slideDistance = (1.0 - animationPercent) * 150;
-
-            return Opacity(
-              opacity: opacity,
-              child: Transform.translate(
-                offset: Offset(slideDistance, 0),
-                child: child,
-              ),
-            );
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-            //menu item
-            child: SizedBox(
-              width: 300, // Thiết lập chiều rộng của nút
-              child: ElevatedButton(
-                onPressed: () {
-                  // Điều hướng tới các trang tương ứng
-                  switch (_menuTitles[i]) {
-                    case 'Thông tin doanh nghiệp':
-                      context.go(Routes.editprofile);
-                      break;
-                    case 'Tài khoản':
-                      context.go(Routes.profile);
-                      break;
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 16,
-                  ),
-                  textStyle: Theme.of(context).textTheme.titleSmall,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18), // Bo góc nút
-                  ),
-                  elevation:
-                      0, // Loại bỏ bóng đổ để phù hợp với Material Design 3
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(
-                      _getIconForMenuTitle(_menuTitles[i]),
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                    const SizedBox(width: 8),
-                    Text(_menuTitles[i]),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
-    }
-
-    // Thêm LogoutButtonBig vào danh sách menu item
-    listItems.add(
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        child: Builder(
-          builder: (context) {
-            return LogoutButtonBig(
-              // Nút đăng xuất
-              viewModel: LogoutViewModel(
-                // Khởi tạo LogoutViewModel
-                authRepository: context.read(), // Lấy authRepository từ context
-                itineraryConfigRepository:
-                    context.read(), // Lấy itineraryConfigRepository từ context
-              ),
-            );
-          },
-        ),
-      ),
-    );
-
-    return listItems;
-  }
-}
-
-IconData _getIconForMenuTitle(String title) {
-  switch (title) {
-    case 'Thông tin doanh nghiệp':
-      return Icons.business;
-    case 'Tài khoản':
-      return Icons.account_circle;
-    default:
-      return Icons.help;
-  }
 }
